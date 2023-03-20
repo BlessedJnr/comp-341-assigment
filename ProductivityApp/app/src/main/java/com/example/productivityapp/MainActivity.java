230205a4a1ea049bc4d17cd0440fa28e9409d929
BlessedJnr <bngorima2002@gmail.com>
2023-03-20 17:01:13 +0200

    added button for testing the project activity

--- ProductivityApp/app/src/main/java/com/example/productivityapp/MainActivity.java
+++ ProductivityApp/app/src/main/java/com/example/productivityapp/MainActivity.java
@@ -2,13 +2,30 @@ package com.example.productivityapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.example.productivityapp.Project.ProjectActivity;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Button testBtn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        testBtn = findViewById(R.id.testBtn);
+
+        testBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, ProjectActivity.class);
+                startActivity(intent);
+            }
+        });
+
     }
 }
