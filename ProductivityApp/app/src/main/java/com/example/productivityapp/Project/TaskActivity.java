a933047061937759f98f74a35f0c1e00e7953945
BlessedJnr <bngorima2002@gmail.com>
2023-03-22 22:05:52 +0200

    made TaskActivity store the recycler view

--- ProductivityApp/app/src/main/java/com/example/productivityapp/Project/TaskActivity.java
+++ ProductivityApp/app/src/main/java/com/example/productivityapp/Project/TaskActivity.java
@@ -1,16 +1,15 @@
 package com.example.productivityapp.Project;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 
 import com.example.productivityapp.R;
 
-public class TasksActivity extends AppCompatActivity {
+public class TaskActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_tasks);
+        setContentView(R.layout.activity_task);
     }
 }
