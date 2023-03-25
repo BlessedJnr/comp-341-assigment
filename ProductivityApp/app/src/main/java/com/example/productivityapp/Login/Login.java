2731055618718c9af4670a68ac1d760094e5a43b
BlessedJnr <bngorima2002@gmail.com>
2023-03-25 20:38:31 +0200

    created log in page and made it laucher activity

---
+++ ProductivityApp/app/src/main/java/com/example/productivityapp/Login/Login.java
@@ -0,0 +1,17 @@
+package com.example.productivityapp.Login;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.productivityapp.R;
+
+public class Login extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
