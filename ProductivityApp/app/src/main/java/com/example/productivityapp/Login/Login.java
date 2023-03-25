fb52112ab46660dc8ff4bc44727bd5dadf13164e
BlessedJnr <bngorima2002@gmail.com>
2023-03-25 20:45:57 +0200

    added binding to the login activity

--- ProductivityApp/app/src/main/java/com/example/productivityapp/Login/Login.java
+++ ProductivityApp/app/src/main/java/com/example/productivityapp/Login/Login.java
@@ -5,13 +5,16 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.example.productivityapp.R;
+import com.example.productivityapp.databinding.ActivityLoginBinding;
 
 public class Login extends AppCompatActivity {
 
+    private ActivityLoginBinding binding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
+        binding = ActivityLoginBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
     }
 }
