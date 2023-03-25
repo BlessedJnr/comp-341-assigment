ec1a9a1405271eb825a983947e5f1fe46143037b
BlessedJnr <bngorima2002@gmail.com>
2023-03-25 21:48:55 +0200

    completed fields for creating project class

--- ProductivityApp/app/src/main/java/com/example/productivityapp/Project/CreateProject.java
+++ ProductivityApp/app/src/main/java/com/example/productivityapp/Project/CreateProject.java
@@ -1,4 +1,30 @@
 package com.example.productivityapp.Project;
 
+import java.util.ArrayList;
+
 public class CreateProject {
+
+    private String projectName;
+    private ArrayList<CreateTasks> tasksList;
+
+    public CreateProject(String projectName, ArrayList<CreateTasks> tasksList) {
+        this.projectName = projectName;
+        this.tasksList = tasksList;
+    }
+
+    public String getProjectName() {
+        return projectName;
+    }
+
+    public void setProjectName(String projectName) {
+        this.projectName = projectName;
+    }
+
+    public ArrayList<CreateTasks> getTasksList() {
+        return tasksList;
+    }
+
+    public void setTasksList(ArrayList<CreateTasks> tasksList) {
+        this.tasksList = tasksList;
+    }
 }
