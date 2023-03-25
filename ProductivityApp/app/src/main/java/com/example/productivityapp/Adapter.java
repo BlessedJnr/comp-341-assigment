174e93beb6c408bea24728c334bf3cd2f86c8f98
Valdesi <116732948+Valdesi@users.noreply.github.com>
2023-03-25 23:41:16 +0200

    Update Added
    hello

--- ProductivityApp/app/src/main/java/com/example/productivityapp/Adapter.java
+++ ProductivityApp/app/src/main/java/com/example/productivityapp/Adapter.java
@@ -1,9 +1,14 @@
 package com.example.productivityapp;
 
 
+import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -11,6 +16,15 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import com.firebase.ui.database.FirebaseRecyclerAdapter;
 import com.firebase.ui.database.FirebaseRecyclerOptions;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.database.FirebaseDatabase;
+import com.orhanobut.dialogplus.DialogPlus;
+import com.orhanobut.dialogplus.ViewHolder;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class Adapter extends FirebaseRecyclerAdapter<Post, Adapter.PostViewholder>
 {
@@ -24,15 +38,134 @@ public class Adapter extends FirebaseRecyclerAdapter<Post, Adapter.PostViewholde
     }
 
     @Override
-    protected void onBindViewHolder(@NonNull Adapter.PostViewholder holder, int i, @NonNull Post post) {
+    protected void onBindViewHolder(@NonNull PostViewholder holder, int position, @NonNull Post post) {
 
-        getRef(i).getKey();
+        String key = getRef(holder.getAdapterPosition()).getKey();
         holder.Fname.setText(post.getName());
         holder.Email.setText(post.getEmail());
         holder.Project.setText(post.getProject());
         holder.Team.setText(post.getTeam());
 
+        holder.Edit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                DialogPlus dialogPlus=DialogPlus.newDialog(context)
+                        .setGravity(Gravity.CENTER)
+                        .setMargin(50,0,50,0)
+                        .setContentHolder(new ViewHolder(R.layout.edit))
+                        .setExpanded(false)
+                        .create();
+
+                View holderView=(LinearLayout) dialogPlus.getHolderView();
+
+                EditText Fname=holderView.findViewById(R.id.name);
+                EditText Email=holderView.findViewById(R.id.email);
+                EditText Team=holderView.findViewById(R.id.team);
+                EditText Project=holderView.findViewById(R.id.project);
+
+                if (Fname != null ) {
+                    Fname.setText(post.getName());
+
+                }
+                Email.setText(post.getEmail());
+                Team.setText(post.getTeam());
+                Project.setText(post.getProject());
+
+
+
+                Button Update=holderView.findViewById(R.id.updatebtn);
+
+                Update.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        Map<String,Object> map=new HashMap<>();
+
+                        map.put("name",Fname.getText().toString());
+                        map.put("email",Email.getText().toString());
+                        map.put("team",Team.getText().toString());
+                        map.put("project",Project.getText().toString());
+
+                        FirebaseDatabase.getInstance().getReference().child("TeamMembers")
+                                .child(key)
+                                .updateChildren(map)
+                                .addOnCompleteListener(new OnCompleteListener<Void>() {
+                                    @Override
+                                    public void onComplete(@NonNull Task<Void> task) {
+                                        dialogPlus.dismiss();
+                                    }
+                                });
+
+                    }
+
+                });
+                dialogPlus.show();
+            }
+        });
     }
+
+/*
+    @Override
+
+    protected void onBindViewHolder(@NonNull PostViewholder holder, int position, @NonNull Post post) {
+
+        getRef(position).getKey();
+        holder.Fname.setText(post.getName());
+        holder.Email.setText(post.getEmail());
+        holder.Project.setText(post.getProject());
+        holder.Team.setText(post.getTeam());
+
+        holder.Edit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                DialogPlus dialogPlus=DialogPlus.newDialog(context)
+                        .setGravity(Gravity.CENTER)
+                        .setMargin(50,0,50,0)
+                        .setContentHolder(new ViewHolder(R.layout.edit))
+                        .setExpanded(false)
+                        .create();
+
+                View holderView=(LinearLayout) dialogPlus.getHolderView();
+
+                EditText Fname=holderView.findViewById(R.id.fullname);
+                EditText Email=holderView.findViewById(R.id.email);
+                EditText Team=holderView.findViewById(R.id.team);
+                EditText Project=holderView.findViewById(R.id.project);
+
+                Fname.setText(post.getName());
+                Email.setText(post.getEmail());
+                Team.setText(post.getTeam());
+                Project.setText(post.getProject());
+
+                Button Update=holderView.findViewById(R.id.updatebtn);
+
+                Update.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        Map<String,Object> map=new HashMap<>();
+
+                        map.put("name",Fname.getText().toString());
+                        map.put("email",Email.getText().toString());
+                        map.put("team",Team.getText().toString());
+                        map.put("project",Project.getText().toString());
+
+                        FirebaseDatabase.getInstance().getReference().child("TeamMembers")
+                                .child(getRef(position).getKey())
+                                .updateChildren(map)
+                                .addOnCompleteListener(new OnCompleteListener<Void>() {
+                                    @Override
+                                    public void onComplete(@NonNull Task<Void> task) {
+                                        dialogPlus.dismiss();
+                                    }
+                                });
+
+                    }
+
+                });
+                dialogPlus.show();
+            }
+        });
+
+    }*/
 
     @NonNull
     @Override
@@ -44,6 +177,7 @@ public class Adapter extends FirebaseRecyclerAdapter<Post, Adapter.PostViewholde
     public class PostViewholder extends RecyclerView.ViewHolder {
 
         TextView Fname,Email,Team,Project;
+        ImageView Edit;
 
         public PostViewholder(@NonNull View itemView) {
             super(itemView);
@@ -52,6 +186,8 @@ public class Adapter extends FirebaseRecyclerAdapter<Post, Adapter.PostViewholde
             Email=itemView.findViewById(R.id.email);
             Team=itemView.findViewById(R.id.team);
             Project=itemView.findViewById(R.id.project);
+
+            Edit=itemView.findViewById(R.id.edit);
         }
     }
 }
