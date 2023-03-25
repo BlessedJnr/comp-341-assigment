af77b63700e9c24decb78c67e8d05f29bd4a13e1
BlessedJnr <bngorima2002@gmail.com>
2023-03-25 19:39:15 +0200

    fixed errors when merging

--- ProductivityApp/app/src/main/java/com/example/productivityapp/MainActivity.java
+++ ProductivityApp/app/src/main/java/com/example/productivityapp/MainActivity.java
@@ -2,7 +2,12 @@ package com.example.productivityapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.example.productivityapp.Project.ProjectActivity;
 
 public class MainActivity extends AppCompatActivity {
 
