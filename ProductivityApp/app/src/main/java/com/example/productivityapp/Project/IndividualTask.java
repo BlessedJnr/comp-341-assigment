161f2b35688cd4290639a6402647b68f3ca99648
BlessedJnr <bngorima2002@gmail.com>
2023-03-27 23:39:12 +0200

    created layout for individual tasks'

---
+++ ProductivityApp/app/src/main/java/com/example/productivityapp/Project/IndividualTask.java
@@ -0,0 +1,16 @@
+package com.example.productivityapp.Project;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.productivityapp.R;
+
+public class IndividualTask extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_individual_task);
+    }
+}
