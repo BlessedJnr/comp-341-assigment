543204281380feeab10f534d48a711ed84f369c5
Kevin <125473461+viizion-03@users.noreply.github.com>
2023-04-02 15:25:06 +0200

    creating structure classes

---
+++ ProductivityApp/app/src/main/java/com/example/productivityapp/Project.java
@@ -0,0 +1,40 @@
+package com.example.productivityapp;
+
+import java.util.List;
+
+
+public class Project {
+    String name;
+    boolean isComplete;
+    List<Task> tasks;
+
+    public Project(String name, boolean status, List<Task> tasks) {
+        this.name = name;
+        this.isComplete = status;
+        this.tasks = tasks;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public boolean isComplete() {
+        return isComplete;
+    }
+
+    public void setIsComplete(boolean complete) {
+        this.isComplete = complete;
+    }
+
+    public List<Task> getTasks() {
+        return tasks;
+    }
+
+    public void setTasks(List<Task> tasks) {
+        this.tasks = tasks;
+    }
+}
