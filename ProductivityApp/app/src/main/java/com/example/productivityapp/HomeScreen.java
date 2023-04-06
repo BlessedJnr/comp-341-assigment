a4e72d083407a2708f2e046792c0c103a4d3c12b
at20000543 <at20000543@studentmail.biust.ac.bw>
2023-04-06 22:40:59 +0200

    Added a recycler view to home screen which displays recent activities

--- ProductivityApp/app/src/main/java/com/example/productivityapp/HomeScreen.java
+++ ProductivityApp/app/src/main/java/com/example/productivityapp/HomeScreen.java
@@ -1,12 +1,21 @@
 package com.example.productivityapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.database.Cursor;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.widget.Toast;
 import android.widget.Toolbar;
 
+import java.util.ArrayList;
+
 public class HomeScreen extends AppCompatActivity {
+    RecyclerView recyclerView;
+    ArrayList<String> projectName,tasks, team;
+    MyAdapter adapter;
 
     Toolbar toolbar;
     @Override
@@ -14,11 +23,40 @@ public class HomeScreen extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         
         setContentView(R.layout.activity_home_screen);
+        projectName = new ArrayList<>();
+        tasks = new ArrayList<>();
+        team = new ArrayList<>();
+        recyclerView = findViewById(R.id.recyclerView);
+
+        adapter = new MyAdapter(this, projectName,tasks,team);
+
+        recyclerView.setAdapter(adapter);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        displaydata();
 
 
 
 
     }
+
+    private void displaydata() {
+        //populate with information from the database
+        /**Cursor cursor = db.getdata();
+        if (cursor.getCount() == 0)
+        {
+            Toast.makeText(HomeScreen.this,"no recent activities",Toast.LENGTH_SHORT).show();
+            return;
+        }
+        else {
+            while (cursor.moveToNext()){
+         **/
+        projectName.add("ProductivityApp");
+        tasks.add("Wireframes,Research");
+        team.add("Tawidza");
+           // }
+        //}
+    }
+
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         int id = item.getItemId();
