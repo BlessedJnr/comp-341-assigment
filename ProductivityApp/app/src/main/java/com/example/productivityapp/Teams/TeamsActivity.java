2942fadf7fbdee2b8b52272a48a29bcd6f4f35da
BlessedJnr <bngorima2002@gmail.com>
2023-04-08 11:51:12 +0200

    made teams allow adding of projects

--- ProductivityApp/app/src/main/java/com/example/productivityapp/Teams/TeamsActivity.java
+++ ProductivityApp/app/src/main/java/com/example/productivityapp/Teams/TeamsActivity.java
@@ -9,8 +9,11 @@ import androidx.recyclerview.widget.RecyclerView;
 import android.os.Bundle;
 import android.os.Handler;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Toast;
 
+import com.example.productivityapp.Project.CreateProject;
+import com.example.productivityapp.R;
 import com.example.productivityapp.databinding.ActivityTeamsBinding;
 import com.google.android.material.bottomsheet.BottomSheetBehavior;
 import com.google.firebase.auth.FirebaseAuth;
@@ -30,9 +33,10 @@ public class TeamsActivity extends AppCompatActivity {
     private ViewTeamsAdapter mAdapter;
     private List<ViewTeamsAdapter.TeamsItem> teamsItems;
     ActivityTeamsBinding binding;
-
     private FirebaseUser user;
-    DatabaseReference currentUserTeamRef;
+    DatabaseReference currentUserTeamRef, currentProjectRef;
+    private ArrayList<String> projectList = new ArrayList<>();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
@@ -45,7 +49,10 @@ public class TeamsActivity extends AppCompatActivity {
         String email = user.getEmail();
         assert email != null;
         String encodedEmail = email.replace(".", ",");
+
+        //get other references to the real time database
         currentUserTeamRef = usersRef.child(encodedEmail).child("Teams");
+        currentProjectRef = FirebaseDatabase.getInstance().getReference("Users").child(encodedEmail).child("Projects");
 
 
         super.onCreate(savedInstanceState);
@@ -60,6 +67,13 @@ public class TeamsActivity extends AppCompatActivity {
         createTeamCardList();
         buildRecylerView();
         retrieveTeam();
+
+        //get all the projects
+        retrieveProjects();
+
+        //add projects to dropdown menu
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<>(this, R.layout.team_project, projectList);
+        binding.autoCompleteTextView.setAdapter(arrayAdapter);
 
         BottomSheetBehavior bottomSheetBehavior = BottomSheetBehavior.from(binding.teamsStandardBtmSheet);
         bottomSheetBehavior.setState(BottomSheetBehavior.STATE_HIDDEN);
@@ -114,7 +128,6 @@ public class TeamsActivity extends AppCompatActivity {
         DatabaseReference newTeamRef = currentUserTeamRef.push();
         newTeamRef.setValue(teams);
     }
-
     private void retrieveTeam() {
         currentUserTeamRef.addValueEventListener(new ValueEventListener() {
 
@@ -142,4 +155,27 @@ public class TeamsActivity extends AppCompatActivity {
             }
         });
     }
+    private void retrieveProjects() {
+        currentProjectRef.addValueEventListener(new ValueEventListener() {
+
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+
+                for (DataSnapshot dataSnapshot : snapshot.getChildren()) {
+                    //get the project object from the snapshot
+                    CreateProject project = dataSnapshot.getValue(CreateProject.class);
+
+                    //add project to the list of project items
+                    assert project != null;
+                    projectList.add(project.getProjectName());
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+                Toast.makeText(getApplicationContext(), "Failed", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
 }
